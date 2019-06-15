6cf2278fd5042fd548bd30b3810a17403e01f66c
Paolo Grisoli <paolo_g84@icloud.com>
2019-06-15 11:17:14 +0200

    First commit

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,31 +1,45 @@
 <template>
   <div>
-    Google snippet preview:
-    <div class="p-metatags__preview">
-      <div class="p-metatags__google-title">{{ model.title || 'Your title' }}</div>
-      <div class="p-metatags__google-link">yoursite.com/example</div>
-      <div class="p-metatags__google-description">{{ model.description || 'Your description' }}</div>
-    </div>
+    <ul>
+      <li v-for="(ingredient, index) in ingredients" :key="index">
+        {{ingredient.quantity}}{{ingredient.unit}} {{ingredient.name}}
+        <button class="uk-button uk-button-small" @click="editIngr(index)">Edit</button>
+        <button class="uk-button uk-button-small" @click="deleteIngr(index)">Delete</button>
+      </li>
+    </ul>
     <div class="uk-form-row">
-      <label>Meta Title</label>
-      <input type="text" placeholder="Your title" v-model="model.title" class="uk-width-1-1">
+      <label>Quantità</label>
+      <input type="text" placeholder="Quantità" v-model="quantity" class="uk-width-1-1">
+      <label>Unità</label>
+      <input type="text" placeholder="Unità" v-model="unit" class="uk-width-1-1">
+      <label>Nome</label>
+      <input type="text" placeholder="Nome ingrediente" v-model="name" class="uk-width-1-1">
     </div>
-
-    <div class="uk-form-row">
-      <label>Meta description</label>
-      <textarea rows="4" placeholder="Your description" v-model="model.description" class="uk-width-1-1"></textarea>
-    </div>
+    <br>
+    <button 
+      class="uk-button uk-button-default add-button"
+      @click="addIngredient">+</button>
   </div>
 </template>
 
 <script>
 export default {
   mixins: [window.Storyblok.plugin],
+  data() {
+    return {
+      ingredients: [],
+      name: '',
+      quantity: '',
+      unit: '',
+      editMode: false,
+      editIndex: null
+    }
+  },
   methods: {
     initWith() {
       return {
         // needs to be equal to your storyblok plugin name
-        plugin: 'my-plugin-name',
+        plugin: 'ingredients',
         title: '',
         description: ''
       }
@@ -33,6 +47,33 @@ export default {
     pluginCreated() {
       // eslint-disable-next-line
       console.log('View source and customize: https://github.com/storyblok/storyblok-fieldtype')
+    },
+    addIngredient() {
+      if (this.editMode) {
+        this.ingredients[this.editIndex].name = this.name
+        this.ingredients[this.editIndex].quantity = this.quantity
+        this.ingredients[this.editIndex].unit = this.unit
+      } else {
+        this.ingredients.push({
+          name: this.name,
+          quantity: this.quantity,
+          unit: this.unit
+        })
+      }
+      this.name = ''
+      this.quantity = ''
+      this.unit = ''
+      this.editMode = false
+    },
+    deleteIngr(index) {
+      this.ingredients.splice(index, 1)
+    },
+    editIngr(index) {
+      this.name = this.ingredients[index].name
+      this.quantity = this.ingredients[index].quantity
+      this.unit = this.ingredients[index].unit
+      this.editMode = true
+      this.editIndex = index
     }
   },
   watch: {
@@ -47,19 +88,7 @@ export default {
 </script>
 
 <style>
-  .p-metatags__google-title {
-    color: blue;
-    text-decoration: underline;
-  }
-
-  .p-metatags__google-link {
-    color: green;
-  }
-
-  .p-metatags__preview {
-    margin: 5px 0 15px;
-    padding: 10px;
-    color: #000;
-    background: #FFF;
-  }
+.add-button {
+  width: 100%;
+}
 </style>
